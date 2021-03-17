4bc46e4aeeb9202f74b6e159c286ad4a7f15ae4c
moi23 <moisesboas@gmail.com>
2021-03-17 00:58:41 -0300

    feat: methods MVC

---
+++ Anotacoes.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * GET - Buscar uma informação dentro do servidor
+ * POST - Inserir uma informação no servidor
+ * PUT - Alterar uma informação no servidor
+ * Patch - Alterar uma informação especifica no servidor
+ * Delete - Deletar uma informação no Servidor
+ *
+ */
